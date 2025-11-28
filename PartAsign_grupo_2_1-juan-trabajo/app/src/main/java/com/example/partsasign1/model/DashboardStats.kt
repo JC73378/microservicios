668f3fa425c6e26b28db21f4f487cbb0814935f9
JC <ju.carmonag@duocuc.cl>
2025-11-28 20:12:37 -0300

    microservicios finalizados

--- PartAsign_grupo_2_1-juan-trabajo/app/src/main/java/com/example/partsasign1/model/DashboardStats.kt
+++
@@ -1,8 +0,0 @@
-package com.example.partsasign1.model
-
-data class DashboardStats(
-    val disponibles: Int,
-    val retirados: Int,
-    val pendientes: Int,
-    val total: Int
-)
--- PartAsign_grupo_2_1-juan-trabajo/app/src/main/java/com/example/partsasign1/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.partsasign1.model
-
-data class User(
-    val id: String,
-    val name: String,
-    val email: String,
-    val role: String
-)
